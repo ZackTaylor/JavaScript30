a88e1ae2174082b432b1138fa7b2d1002733dd05
Zack Tylor <taylor.zackory@gmail.com>
2017-10-03 17:55:07 -0500

    complete js drum kit challenge.

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -1,4 +1,23 @@
 (function() {
+  function playSound(event) {
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+    if(!audio) return; // stop if invalid key pressed
+    audio.currentTime = 0; // Rewind to start so audio plays every keypress
+    audio.play();
 
+    key.classList.add('playing');
+  }
 
+  function removeTransition(event) {
+    if (event.propertyName !== 'transform') return; // skip if elem hasnt been transformed
+    this.classList.remove('playing');
+  }
+
+  (function() {
+    const keys = document.querySelectorAll('.key');
+    keys.forEach( key => key.addEventListener('transitionend', removeTransition));
+  })();
+
+  window.addEventListener('keydown', playSound);
 })();
